1d5a92710091e0128b3c7a0c25822e8556923533
Harald Markus Wirth <harald.wirth@gmx.at>
2023-11-19 21:30:04 +0100

    Add HTML Webpack plugin

---
+++ TODO.md
@@ -0,0 +1,4 @@
+TODO
+====
+[ ] Figure out how to copy CSS files as is to dist
+[?] TODO might be better replaced with Github Issues
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
+// Enable ES6 imports
 // https://flaviocopes.com/fix-dirname-not-defined-es-module-scope/
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,6 +14,12 @@ export default {
     static: './dist',
   },
   entry: './src/main.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+        title: 'Output Management',
+        template: './src/index.html',
+    })
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
